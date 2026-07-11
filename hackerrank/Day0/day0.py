b40825feec02fc05e7c43b7e2a804395a5b016e8
Jota <minayajunior30@gmail.com>
2026-07-11 14:33:36 +0000

    Modificacion nueva para el dia 0  de hackerrrank del lenguaje python3

--- hackerrank/Day0/day0.py
+++ hackerrank/Day0/day0.py
@@ -1,7 +1,7 @@
-inputString = input() # get a line of input from stdin and save it to our variable
+Mensaje_usuario = input('Escriba un mensaje: ') # get a line of input from stdin and save it to our variable
 
 # Your first line of output goes here
-print('Hello, World.')
+print('Prueba de programa')
 
 # Write the second line of output
-print(inputString)
+print (f'Este es el mensaje escrito por el usuario: {Mensaje_usuario}')
